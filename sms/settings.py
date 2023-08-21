92e166ed89dad5964cb68e0294cd9e86385f5d1d
Alain Miki <59676620+alainmiki@users.noreply.github.com>
2023-08-21 23:31:47 +0100

    remove whitenoise

--- sms/settings.py
+++ sms/settings.py
@@ -233,13 +233,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT="media"
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STORAGES = {
-    # ...
-    "static": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STORAGES = {
+#     # ...
+#     "static": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
